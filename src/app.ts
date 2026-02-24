f705b498de9ae6015fa3e880a3fc972d5115f394
Heber Robert <euheber1@gmail.com>
2026-02-24 20:39:47 -0300

    install: @prisma/client dependecies

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,14 @@
 import fastify from "fastify"
 import {PrismaClient} from "../generated/prisma/client.ts"
+import { PrismaPg } from "@prisma/adapter-pg"
+import { env } from "./.env/index.ts"
 
+
+const connectionString = `${env.DATABASE_URL}`
+const adapter = new PrismaPg({connectionString})
 
 export const app = fastify()
-const prisma = new PrismaClient()
+
+const prisma = new PrismaClient({adapter})
+
+export {prisma}
--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import { app } from "./app.ts";
 import {env} from "@/.env/index.ts"
 
 app.listen({port: env.PORT, host: "0.0.0.0"}).then(()=>{
-    
+
     console.log(`Server online on port ${env.PORT}`);
      
 })
